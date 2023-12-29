c24bd1af5965cd8266f68ba7bd00633e313aff60
EsraTosun <esratosun171@gmail.com>
2023-12-29 12:34:24 +0300

    first commit

---
+++ SaglikWebUygulamasi1/Controllers/DefaultController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SaglikWebUygulamasi1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string inputEmail, string inputPassword)
+        {
+            // Kullanıcı adı ve şifre kontrolü yapılacak
+            // Eğer giriş başarılıysa, kullanıcıyı ana sayfaya yönlendirilebilir
+            // Eğer giriş başarısızsa, hata mesajı eklenerek tekrar giriş sayfasına yönlendirilebilir
+            // Kullanıcı giriş kontrolü burada yapılır.
+            if (KullaniciGirisKontrol(inputEmail, inputPassword))
+            {
+                // Giriş başarılı ise, kullanıcıyı Index action'ına yönlendir.
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Giriş başarısız ise, kullanıcıyı Login sayfasında tut.
+                return View();
+            }
+            //return RedirectToAction("Index", "Home"); // Örnek bir yönlendirme
+        }  
+        /*public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        private bool KullaniciGirisKontrol(string username, string password)
+        {
+            // Kullanıcı giriş kontrolü gerçekleştirilir.
+            // Bu metot, kullanıcı girişi başarılı ise true, aksi halde false dönmelidir.
+            // Giriş kontrolü işlemleri burada gerçekleştirilir.
+            return true; // Örnek olarak her zaman başarılı dönüş yapalım.
+        }
+    }
+}
---
+++ SaglikWebUygulamasi1/Files/js/Login/login.js
@@ -0,0 +1,18 @@
+﻿<script>
+document.getElementById("loginButton").onclick = function() {
+    // AJAX kullanarak sunucu tarafındaki Login action'ını çağırma
+    $.ajax({
+        url: '/Default/Login', // Action'ın URL'sini doğru olarak belirtin
+        type: 'POST',
+        data: { username: 'kullaniciAdi', password: 'sifre' }, // Kullanıcı adı ve şifreyi uygun şekilde belirtin
+        success: function(result) {
+            // Başarılı cevap geldiğinde, gelen verileri değerlendirin (isteğe bağlı)
+            console.log(result);
+        },
+        error: function(error) {
+            // Hata durumunda işlemleri gerçekleştirin (isteğe bağlı)
+            console.error(error);
+        }
+    });
+};
+</script>
